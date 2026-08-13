e90f66c4421190da9ecf8901d54209d669969aa6
GabbFlor <gabriel.florindo.souza46@gmail.com>
2026-08-12 21:19:50 -0300

    feat: subindo um novo projeto da matéria de aplicações web

---
+++ aplicacoes_web/imagem_pecas/README.md
@@ -0,0 +1,10 @@
+# Imagem peças
+ 
+Esse projeto está previsto para ter front-end e back-end.
+
+## Estrutura:
+
+```text
+aplicacoes_web/imagem_pecas/
+├── projetoImagemPecas_backend/   # API / Regras de negócio e persistência
+└── projetoImagemPecas_frontend/  # Interface de usuário (em desenvolvimento)
---
+++ aplicacoes_web/imagem_pecas/projetoImagemPecas_backend/src/main/java/com/flow3rz/projetoImagemPecas/ProjetoImagemPecasApplication.java
@@ -0,0 +1,13 @@
+package com.flow3rz.projetoImagemPecas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoImagemPecasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoImagemPecasApplication.class, args);
+	}
+
+}
